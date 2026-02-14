a1ed7bf600c21630cf222fec059d9d955f225407
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-02-14 18:33:04 -0300

    Form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import Header from "./Header";
+import Main from "./MainComponent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/MainComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g oregano"
+          aria-label="Add ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+    </main>
+  );
+};
+
+export default Main;
