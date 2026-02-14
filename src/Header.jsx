0f1e21247e6cc6b1741f7e737180af67da2e0eee
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-02-14 18:12:39 -0300

    Header created

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="src/assets/chef-claude-icon.png" alt="" />
+      <h1> Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
