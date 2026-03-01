05ddb516217abc16d96ecec90437da9d288c389e
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-03-01 11:49:00 +0100

    Conditionals

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import { useState } from "react";
 
 const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [newIngredient, setNewIngredient] = useState("");
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  const handleNewIngredient = (event) => {
+    setNewIngredient(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+  };
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="e.g oregano"
           aria-label="Add ingredient"
+          name="ingredient"
+          value={newIngredient}
+          onChange={(e) => {
+            handleNewIngredient(e);
+          }}
         />
         <button>Add ingredient</button>
       </form>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   );
 };
